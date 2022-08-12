94359e758380cd255df2592097357072f9d878c7
GSWXXN <819269088@qq.com>
2022-08-13 00:06:38 +0800

    新增 支持将设备作为镜像或扩展屏幕

--- app/src/main/java/com/gswxxn/unlockmilink/hook/HookEntry.kt
+++ app/src/main/java/com/gswxxn/unlockmilink/hook/HookEntry.kt
@@ -3,6 +3,7 @@ package com.gswxxn.unlockmilink.hook
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.xposed.bridge.event.YukiXposedEvent
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
@@ -14,6 +15,13 @@ class HookEntry : IYukiHookXposedInit {
     }
 
     override fun onHook() = encase {
+        loadApp("com.xiaomi.mi_connect_service") {
+            onAppLifecycle {
+                onCreate {
+                    "miui.os.Build".clazz.field { name = "IS_TABLET" }.get().set(true)
+                }
+            }
+        }
         loadApp("com.milink.service", MiLinkHooker())
         loadApp("com.xiaomi.mirror", MirrorHooker())
     }
